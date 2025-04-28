f98ee8dc1b2f68fb465e4f90c840dc2c6634eb11
yunjuKimm <yunju.tech@gmail.com>
2025-04-28 22:53:00 +0900

    feat: Dibs dto validation추가

--- backend/src/main/java/com/team5/backend/domain/dibs/dto/DibsCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/dibs/dto/DibsCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.team5.backend.domain.dibs.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class DibsCreateReqDto {
+
+    @NotNull(message = "회원 ID는 필수입니다.")
     private Long memberId;
+
+    @NotNull(message = "상품 ID는 필수입니다.")
     private Long productId;
 }
