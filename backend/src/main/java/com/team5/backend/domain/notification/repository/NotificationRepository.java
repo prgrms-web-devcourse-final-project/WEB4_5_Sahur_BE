6cddc52e0f5f437ba5023c95b1df55aace52c151
SDHSeoulTech <113351553+SDHSeoulTech@users.noreply.github.com>
2025-04-28 12:16:42 +0900

    Feat/#11 알림 레이어드 아키텍처 구현 (#18)
    * feat: app, dto, entity,exception
    
    * feat: 공동구매 entity 구현
    
    * feat: 공동구매 상태 enum 구현
    
    ONGOING(모집중), CLOSED(모집 종료)
    
    * feat: 공동구매 리포지토리 구현
    
    * feat: 공동구매 생성, 수정 요청 dto, 응답 dto 구현
    
    응답 dto는 공동구매 엔티티의 모든값 포함
    
    * feat: 공동구매 서비스 구현
    
    * feat: 공동구매 컨트롤러 구현
    
    * comment: 공동구매 상태 enum 주석 추가
    
    * change to yml, add h2
    
    * feat: 리뷰 엔티티 구현
    
    * feat: 리뷰 작성/수정 요청, 응답 dto 구현
    
    * feat: 리뷰 리포지토리 구현
    
    * feat: 리뷰 CRUD 서비스 구현
    
    * feat: 리뷰 컨트롤러 구현
    
    * style: 공동구매 컨트롤러 RequestMapping 통일
    
    * feat: 알림 엔티티 구현
    
    * feat: 알림 type enum 구현
    
    * feat: 알림 작성/수정 요청, 응답 dto 구현
    
    * feat: 알림 리포지토리 구현
    
    * feat: 알림 서비스 구현
    
    * feat: 알림 컨트롤러 구현
    
    ---------
    
    Co-authored-by: linedj <eunjun998@gmail.com>
    Co-authored-by: ko jaehyun <139321389+kozae00@users.noreply.github.com>

---
+++ backend/src/main/java/com/team5/backend/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.team5.backend.domain.notification.repository;
+
+import com.team5.backend.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
