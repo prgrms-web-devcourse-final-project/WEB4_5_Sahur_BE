116ef3099b86a775b2b7de046d504374515ceb19
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-28 11:22:19 +0900

    feat: 리뷰 작성/수정 요청, 응답 dto 구현

---
+++ backend/src/main/java/com/team5/backend/domain/review/dto/ReviewCreateReqDto.java
@@ -0,0 +1,14 @@
+package com.team5.backend.domain.review.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewCreateReqDto {
+    private Long memberId;
+    private Long productId;
+    private String comment;
+    private Integer rate;
+    private String imageUrl;
+}
