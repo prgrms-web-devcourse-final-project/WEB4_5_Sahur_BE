7c2fd804d32b4c3c08877411cae318b49d2fca61
yunjuKimm <yunju.tech@gmail.com>
2025-04-28 15:16:52 +0900

    feat: 카테고리 repository 구현

---
+++ backend/src/main/java/com/team5/backend/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.team5.backend.domain.category.repository;
+
+import com.team5.backend.domain.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
