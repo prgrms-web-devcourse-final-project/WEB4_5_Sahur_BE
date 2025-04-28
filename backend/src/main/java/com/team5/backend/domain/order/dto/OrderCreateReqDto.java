d460bfe6731bdc827e6e809e34d02b48e200f4bc
jxxngin <wwd0203@daum.net>
2025-04-28 16:54:14 +0900

    feat: 주문 생성 및 수정 요청용 DTO 검증 어노테이션 적용

--- backend/src/main/java/com/team5/backend/domain/order/dto/OrderCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/order/dto/OrderCreateReqDto.java
@@ -1,10 +1,18 @@
 package com.team5.backend.domain.order.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class OrderCreateReqDto {
+	@NotNull
 	private Long memberId;
+
+	@NotNull
 	private Long groupBuyId;
+
+	@NotNull
+	@Min(1)
 	private Integer quantity;
 }
--- backend/src/main/java/com/team5/backend/domain/order/dto/OrderUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/order/dto/OrderUpdateReqDto.java
@@ -1,10 +1,15 @@
 package com.team5.backend.domain.order.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class OrderUpdateReqDto {
+	@NotNull
+	@Min(1)
 	private Integer quantity;
+
 	private DeliveryInfo delivery;
 
 	@Getter
