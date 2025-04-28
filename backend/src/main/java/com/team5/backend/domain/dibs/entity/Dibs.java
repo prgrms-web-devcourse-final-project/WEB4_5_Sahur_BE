c214d00fbbc15ad919d8d8d287fe281d3643a556
yunjuKimm <yunju.tech@gmail.com>
2025-04-28 16:07:40 +0900

    feat:관심상품 엔티티 구현

---
+++ backend/src/main/java/com/team5/backend/domain/dibs/entity/Dibs.java
@@ -0,0 +1,27 @@
+package com.team5.backend.domain.dibs.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Dibs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Dibs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dibsId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean status;
+}
