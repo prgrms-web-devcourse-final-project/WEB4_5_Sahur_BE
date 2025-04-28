f4d5793cd85edcb06262dfae55c46982bbab1db5
jxxngin <wwd0203@daum.net>
2025-04-28 17:57:21 +0900

    feat: 주문 수정 요청 dto에 배송지 입력값 검증 어노테이션 추가

--- backend/src/main/java/com/team5/backend/domain/order/dto/OrderUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/order/dto/OrderUpdateReqDto.java
@@ -1,6 +1,7 @@
 package com.team5.backend.domain.order.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -14,7 +15,10 @@ public class OrderUpdateReqDto {
 
 	@Getter
 	public static class DeliveryInfo {
+		@NotBlank(message = "주소는 필수 입력 항목입니다.")
 		private String address;
+
+		@NotBlank(message = "연락처는 필수 입력 항목입니다.")
 		private String contact;
 	}
 }
