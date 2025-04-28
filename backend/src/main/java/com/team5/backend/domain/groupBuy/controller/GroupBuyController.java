c8b8cbd5fde442d07a012172d10aa6867ebe4929
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-28 21:56:45 +0900

    feat: 공동구매 컨트롤러 페이징, 사이즈, 정렬순서 반영 및 마감임박 반환 기능 구현

--- backend/src/main/java/com/team5/backend/domain/groupBuy/controller/GroupBuyController.java
+++ backend/src/main/java/com/team5/backend/domain/groupBuy/controller/GroupBuyController.java
@@ -4,8 +4,11 @@ import com.team5.backend.domain.groupBuy.dto.GroupBuyCreateReqDto;
 import com.team5.backend.domain.groupBuy.dto.GroupBuyResDto;
 import com.team5.backend.domain.groupBuy.dto.GroupBuyUpdateReqDto;
 import com.team5.backend.domain.groupBuy.entity.GroupBuy;
+import com.team5.backend.domain.groupBuy.entity.GroupBuySortField;
 import com.team5.backend.domain.groupBuy.service.GroupBuyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +28,24 @@ public class GroupBuyController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GroupBuyResDto>> getAllGroupBuys() {
-        List<GroupBuyResDto> responses = groupBuyService.getAllGroupBuys();
+    public ResponseEntity<List<GroupBuyResDto>> getAllGroupBuys(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size,
+            @RequestParam(value = "sortField", defaultValue = "LATEST") GroupBuySortField sortField) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        List<GroupBuyResDto> responses = groupBuyService.getAllGroupBuys(pageable, sortField);
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/closing")
+    public ResponseEntity<List<GroupBuyResDto>> getClosingGroupBuys(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size,
+            @RequestParam(value = "sortField", defaultValue = "LATEST") GroupBuySortField sortField) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        List<GroupBuyResDto> responses = groupBuyService.getTodayDeadlineGroupBuys(pageable, sortField);
         return ResponseEntity.ok(responses);
     }
 
