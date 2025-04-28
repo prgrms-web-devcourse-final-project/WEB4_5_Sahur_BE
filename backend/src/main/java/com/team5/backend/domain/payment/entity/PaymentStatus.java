e652c8ceea66141b70d8878303d8e5169b64b43d
jxxngin <wwd0203@daum.net>
2025-04-28 12:34:19 +0900

    docs: PaymentStatus enum에 설명 주석 추가

--- backend/src/main/java/com/team5/backend/domain/payment/entity/PaymentStatus.java
+++ backend/src/main/java/com/team5/backend/domain/payment/entity/PaymentStatus.java
@@ -1,5 +1,11 @@
 package com.team5.backend.domain.payment.entity;
 
+/**
+ * 결제 상태를 나타내는 Enum.
+ * - REQUESTED: 결제 요청됨
+ * - CONFIRMED: 결제 완료됨
+ * - CANCELED: 결제 취소됨
+ */
 public enum PaymentStatus {
 	REQUESTED,
 	CONFIRMED,
