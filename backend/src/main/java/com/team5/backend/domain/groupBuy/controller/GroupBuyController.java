bf20c25456d1f8e257c27181de4f4976ce6361f3
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-29 17:09:46 +0900

    refactor: 공동구매 컨트롤러 List -> Page 변환

--- backend/src/main/java/com/team5/backend/domain/groupBuy/controller/GroupBuyController.java
+++ backend/src/main/java/com/team5/backend/domain/groupBuy/controller/GroupBuyController.java
@@ -4,17 +4,14 @@ import com.team5.backend.domain.groupBuy.dto.GroupBuyCreateReqDto;
 import com.team5.backend.domain.groupBuy.dto.GroupBuyResDto;
 import com.team5.backend.domain.groupBuy.dto.GroupBuyStatusResDto;
 import com.team5.backend.domain.groupBuy.dto.GroupBuyUpdateReqDto;
-import com.team5.backend.domain.groupBuy.entity.GroupBuy;
 import com.team5.backend.domain.groupBuy.entity.GroupBuySortField;
 import com.team5.backend.domain.groupBuy.service.GroupBuyService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/groupBuy")
@@ -30,24 +27,24 @@ public class GroupBuyController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GroupBuyResDto>> getAllGroupBuys(
+    public ResponseEntity<Page<GroupBuyResDto>> getAllGroupBuys(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "5") int size,
             @RequestParam(value = "sortField", defaultValue = "LATEST") GroupBuySortField sortField) {
 
         Pageable pageable = PageRequest.of(page, size);
-        List<GroupBuyResDto> responses = groupBuyService.getAllGroupBuys(pageable, sortField);
+        Page<GroupBuyResDto> responses = groupBuyService.getAllGroupBuys(pageable, sortField);
         return ResponseEntity.ok(responses);
     }
 
     @GetMapping("/closing")
-    public ResponseEntity<List<GroupBuyResDto>> getClosingGroupBuys(
+    public ResponseEntity<Page<GroupBuyResDto>> getClosingGroupBuys(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "5") int size,
             @RequestParam(value = "sortField", defaultValue = "LATEST") GroupBuySortField sortField) {
 
         Pageable pageable = PageRequest.of(page, size);
-        List<GroupBuyResDto> responses = groupBuyService.getTodayDeadlineGroupBuys(pageable, sortField);
+        Page<GroupBuyResDto> responses = groupBuyService.getTodayDeadlineGroupBuys(pageable, sortField);
         return ResponseEntity.ok(responses);
     }
 
@@ -83,15 +80,14 @@ public class GroupBuyController {
     }
 
     @GetMapping("/members/{memberId}")
-    public ResponseEntity<List<GroupBuyResDto>> getGroupBuysByMemberId(
+    public ResponseEntity<Page<GroupBuyResDto>> getGroupBuysByMemberId(
             @PathVariable Long memberId,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "5") int size,
             @RequestParam(value = "sortField", defaultValue = "LATEST") GroupBuySortField sortField) {
 
         Pageable pageable = PageRequest.of(page, size);
-        List<GroupBuyResDto> responses = groupBuyService.getGroupBuysByMemberId(memberId, pageable, sortField);
+        Page<GroupBuyResDto> responses = groupBuyService.getGroupBuysByMemberId(memberId, pageable, sortField);
         return ResponseEntity.ok(responses);
     }
-
 }
