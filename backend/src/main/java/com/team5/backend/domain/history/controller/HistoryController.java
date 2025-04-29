c0a4cf043f3c6395b666f61af8245ea2df1820cc
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-29 17:15:24 +0900

    refactor: 구매내역 컨트롤러 List -> Page 변환

--- backend/src/main/java/com/team5/backend/domain/history/controller/HistoryController.java
+++ backend/src/main/java/com/team5/backend/domain/history/controller/HistoryController.java
@@ -5,10 +5,11 @@ import com.team5.backend.domain.history.dto.HistoryResDto;
 import com.team5.backend.domain.history.dto.HistoryUpdateReqDto;
 import com.team5.backend.domain.history.service.HistoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/histories")
@@ -24,8 +25,12 @@ public class HistoryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<HistoryResDto>> getAllHistories() {
-        List<HistoryResDto> responses = historyService.getAllHistories();
+    public ResponseEntity<Page<HistoryResDto>> getAllHistories(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<HistoryResDto> responses = historyService.getAllHistories(pageable);
         return ResponseEntity.ok(responses);
     }
 
