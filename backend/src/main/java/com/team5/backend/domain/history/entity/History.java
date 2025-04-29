edeedc141078963e967dc1818420e6038d3a36c2
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-29 20:25:55 +0900

    feat: 구매내역에 외래키 주문id 추가

--- backend/src/main/java/com/team5/backend/domain/history/entity/History.java
+++ backend/src/main/java/com/team5/backend/domain/history/entity/History.java
@@ -2,6 +2,7 @@ package com.team5.backend.domain.history.entity;
 
 import com.team5.backend.domain.groupBuy.entity.GroupBuy;
 import com.team5.backend.domain.member.member.entity.Member;
+import com.team5.backend.domain.order.entity.Order;
 import com.team5.backend.domain.product.entity.Product;
 import jakarta.persistence.*;
 import lombok.*;
@@ -29,6 +30,11 @@ public class History {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "groupBuyId", nullable = false)
     private GroupBuy groupBuy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orderId", nullable = false)
+    private Order order;
+
 
     @Column(nullable = false)
     private Boolean writable = false; // 작성 가능 여부
