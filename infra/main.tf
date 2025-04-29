80c264fd8f3f89899114cd69d890727d9669e461
linedj <eunjun998@gmail.com>
2025-04-29 23:40:24 +0900

    feat: terraform setting

---
+++ infra/main.tf
@@ -0,0 +1,262 @@
+terraform {
+  // aws 라이브러리 불러옴
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# AWS 설정 시작
+provider "aws" {
+  region = var.region
+}
+# AWS 설정 끝
+
+
+# VPC 설정 시작
+resource "aws_vpc" "team05-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
+}
+
+# Subnet 설정 시작
+resource "aws_subnet" "team05-subnet" {
+  vpc_id                  = aws_vpc.team05-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-subnet"
+  }
+}
+
+
+resource "aws_internet_gateway" "team05-igw" {
+  vpc_id = aws_vpc.team05-vpc.id
+
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "team05-rt" {
+  vpc_id = aws_vpc.team05-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.team05-igw.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rt"
+  }
+}
+
+resource "aws_route_table_association" "team05-association" {
+  subnet_id      = aws_subnet.team05-subnet.id
+  route_table_id = aws_route_table.team05-rt.id
+}
+
+resource "aws_security_group" "team05-sg" {
+  name = "${var.prefix}-sg"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = aws_vpc.team05-vpc.id
+
+  tags = {
+    Name = "${var.prefix}-sg"
+  }
+}
+
+# EC2 역할 생성
+resource "aws_iam_role" "team05-ec2-role" {
+  name = "${var.prefix}-ec2-role"
+
+  # 이 역할에 대한 신뢰 정책 설정. EC2 서비스가 이 역할을 가정할 수 있도록 설정
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
+
+# EC2 역할에 AmazonS3FullAccess 정책을 부착
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.team05-ec2-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# EC2 역할에 AmazonEC2RoleforSSM 정책을 부착
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.team05-ec2-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+# IAM 인스턴스 프로파일 생성
+resource "aws_iam_instance_profile" "team05-instance-profile" {
+  name = "${var.prefix}-instance-profile"
+  role = aws_iam_role.team05-ec2-role.name
+}
+
+locals {
+  ec2_user_data_base = <<-END_OF_FILE
+#!/bin/bash
+# 가상 메모리 4GB 설정
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sh -c 'echo "/swapfile swap swap defaults 0 0" >> /etc/fstab'
+
+# 도커 설치 및 실행/활성화
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+
+# 도커 네트워크 생성
+docker network create common
+
+# nginx 설치
+docker run -d \
+  --name npm_1 \
+  --restart unless-stopped \
+  --network common \
+  -p 80:80 \
+  -p 443:443 \
+  -p 81:81 \
+  -e TZ=Asia/Seoul \
+  -v /dockerProjects/npm_1/volumes/data:/data \
+  -v /dockerProjects/npm_1/volumes/etc/letsencrypt:/etc/letsencrypt \
+  jc21/nginx-proxy-manager:latest
+
+# redis 설치
+docker run -d \
+  --name=redis_1 \
+  --restart unless-stopped \
+  --network common \
+  -p 6379:6379 \
+  -e TZ=Asia/Seoul \
+  redis --requirepass ${var.password_1}
+
+# mysql 설치
+docker run -d \
+  --name mysql_1 \
+  --restart unless-stopped \
+  -v /dockerProjects/mysql_1/volumes/var/lib/mysql:/var/lib/mysql \
+  -v /dockerProjects/mysql_1/volumes/etc/mysql/conf.d:/etc/mysql/conf.d \
+  --network common \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=${var.password_1} \
+  -e TZ=Asia/Seoul \
+  mysql:latest
+
+# MySQL 컨테이너가 준비될 때까지 대기
+echo "MySQL이 기동될 때까지 대기 중..."
+until docker exec mysql_1 mysql -uroot -p${var.password_1} -e "SELECT 1" &> /dev/null; do
+  echo "MySQL이 아직 준비되지 않음. 5초 후 재시도..."
+  sleep 5
+done
+echo "MySQL이 준비됨. 초기화 스크립트 실행 중..."
+
+docker exec mysql_1 mysql -uroot -p${var.password_1} -e "
+CREATE USER 'team05'@'%' IDENTIFIED WITH caching_sha2_password BY '${var.password_1}';
+
+GRANT ALL PRIVILEGES ON *.* TO 'team05'@'%';
+
+CREATE DATABASE glog_prod;
+
+FLUSH PRIVILEGES;
+"
+
+echo "${var.github_access_token_1}" | docker login ghcr.io -u ${var.github_access_token_1_owner} --password-stdin
+
+END_OF_FILE
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "team05-main" {
+  # 사용할 AMI ID
+  ami = data.aws_ami.latest_amazon_linux.id
+  # EC2 인스턴스 유형
+  instance_type = "t3.micro"
+  # 사용할 서브넷 ID
+  subnet_id = aws_subnet.team05-subnet.id
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids = [aws_security_group.team05-sg.id]
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.team05-instance-profile.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2"
+  }
+
+  # 루트 볼륨 설정
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 12 # 볼륨 크기를 12GB로 설정
+  }
+
+  user_data = <<-EOF
+${local.ec2_user_data_base}
+EOF
+}
---
+++ infra/secrets.tf
@@ -0,0 +1,14 @@
+variable "password_1" {
+  description = "password_1"
+  default     = "!!tkgnfm05"
+}
+
+variable "github_access_token_1" {
+  description = "github_access_token_1, read:packages only"
+  default     = "깃허브 토큰입력"
+}
+
+variable "github_access_token_1_owner" {
+  description = "github_access_token_1_owner"
+  default     = "깃허브 엑세스 토큰 주인 닉네임"
+}
