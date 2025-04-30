497fc27add4567a136524bdaa68c0d7a8624c12c
jxxngin <wwd0203@daum.net>
2025-04-30 22:19:11 +0900

    refactor: 예외 구조 변경에 따라 컨트롤러 응답 로직 단순화

--- backend/src/main/java/com/team5/backend/domain/payment/controller/PaymentController.java
+++ backend/src/main/java/com/team5/backend/domain/payment/controller/PaymentController.java
@@ -26,25 +26,16 @@ public class PaymentController {
 
 	@PostMapping("/confirm")
 	public RsData<?> confirmPayment(@RequestBody @Valid ConfirmReqDto request) {
-		boolean isConfirm = tossService.confirmPayment(request);
-
-		if (isConfirm) {
-			paymentService.savePayment(request.getOrderId(), request.getPaymentKey());
-			return new RsData<>("200", "결제에 성공했습니다.");
-		} else {
-			return new RsData<>("400-1", "결제에 실패했습니다.");
-		}
+		tossService.confirmPayment(request);
+		paymentService.savePayment(request.getOrderId(), request.getPaymentKey());
+		return new RsData<>("200", "결제에 성공했습니다.");
 	}
 
 	@GetMapping("/{paymentId}")
 	public RsData<PaymentResDto> getPaymentInfo(@PathVariable("paymentId") String paymentId) {
-		try {
-			String paymentKey = paymentService.getPaymentKey(paymentId);
-			PaymentResDto dto = tossService.getPaymentInfoByPaymentKey(paymentKey);
-			return new RsData<>("200", "결제 정보를 불러왔습니다.", dto);
-		} catch (RuntimeException e) {
-			return new RsData<>("404-1", "결제 정보를 찾을 수 없습니다.");
-		}
+		String paymentKey = paymentService.getPaymentKey(paymentId);
+		PaymentResDto dto = tossService.getPaymentInfoByPaymentKey(paymentKey);
+		return new RsData<>("200", "결제 정보를 불러왔습니다.", dto);
 	}
 
 	@PostMapping("/{orderId}/cancel")
@@ -52,18 +43,9 @@ public class PaymentController {
 		@PathVariable("orderId") Long orderId,
 		@RequestBody @Valid CancelReqDto request
 	) {
-		try {
-			String paymentKey = paymentService.getPaymentKeyByOrder(orderId);
-			boolean isCanceled = tossService.cancelPayment(paymentKey, request.getCancelReason());
-
-			if (isCanceled) {
-				return new RsData<>("200", "결제가 성공적으로 취소되었습니다.");
-			} else {
-				return new RsData<>("400-2", "결제 취소에 실패했습니다.");
-			}
-		} catch (IllegalArgumentException e) {
-			return new RsData<>("404-1", "결제 정보를 찾을 수 없습니다.");
-		}
+		String paymentKey = paymentService.getPaymentKeyByOrder(orderId);
+		tossService.cancelPayment(paymentKey, request.getCancelReason());
+		return new RsData<>("200", "결제가 성공적으로 취소되었습니다.");
 	}
 
 	@GetMapping("/members/{memberId}")
