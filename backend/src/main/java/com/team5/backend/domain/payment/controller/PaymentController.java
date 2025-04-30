5ee11ed842572a9a3a95b5cffe4316ff7724ef0e
jxxngin <wwd0203@daum.net>
2025-04-30 11:11:04 +0900

    feat: 결제 내역 전체 조회 API 구현

--- backend/src/main/java/com/team5/backend/domain/payment/controller/PaymentController.java
+++ backend/src/main/java/com/team5/backend/domain/payment/controller/PaymentController.java
@@ -1,13 +1,14 @@
 package com.team5.backend.domain.payment.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import com.team5.backend.domain.payment.dto.CancelReqDto;
 import com.team5.backend.domain.payment.dto.ConfirmReqDto;
 import com.team5.backend.domain.payment.dto.PaymentResDto;
-import com.team5.backend.domain.payment.entity.Payment;
 import com.team5.backend.domain.payment.service.PaymentService;
 import com.team5.backend.domain.payment.service.TossService;
 import com.team5.backend.global.dto.RsData;
@@ -66,7 +67,13 @@ public class PaymentController {
 	}
 
 	@GetMapping("/members/{memberId}")
-	public List<Payment> getPaymentsByMember(@PathVariable Long memberId) {
-		return paymentService.getPaymentsByMember(memberId);
+	public RsData<Page<PaymentResDto>> getPaymentsByMember(
+		@PathVariable Long memberId,
+		@PageableDefault(size = 5, sort = "paymentId", direction = Sort.Direction.DESC) Pageable pageable
+		) {
+		Page<String> paymentKeys = paymentService.getPaymentKeysByMember(memberId, pageable);
+		Page<PaymentResDto> result = paymentService.getPaymentsByKeysAsync(paymentKeys);
+
+		return new RsData<>("200", "결제 내역을 비동기로 조회했습니다.", result);
 	}
 }
