3c16ea12ce41e9ec9cfa47cb2b0ffee129f604f7
SDHSeoulTech <dongseoultech@gmail.com>
2025-04-30 09:33:31 +0900

    refactor: 관리자 컨트롤러에 @PreAuthorize("hasRole('ADMIN')") 추가

--- backend/src/main/java/com/team5/backend/domain/member/admin/controller/AdminController.java
+++ backend/src/main/java/com/team5/backend/domain/member/admin/controller/AdminController.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final AdminService adminService;
