8d7dbd08edfb6abdb1d1fd2cad960babeded8b90
yunjuKimm <yunju.tech@gmail.com>
2025-04-30 11:31:58 +0900

    refactor: product dto 수정

--- backend/src/main/java/com/team5/backend/domain/product/dto/ProductCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/product/dto/ProductCreateReqDto.java
@@ -1,12 +1,14 @@
 package com.team5.backend.domain.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCreateReqDto {
-    private Long memberId;
+    private Long categoryId;
     private String title;
     private String description;
     private String imageUrl;
--- backend/src/main/java/com/team5/backend/domain/product/dto/ProductUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/product/dto/ProductUpdateReqDto.java
@@ -1,15 +1,17 @@
 package com.team5.backend.domain.product.dto;
 
-import com.team5.backend.domain.product.entity.Product.ProductStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductUpdateReqDto {
     private String title;
     private String description;
     private String imageUrl;
     private Integer price;
-    private ProductStatus status;
 }
