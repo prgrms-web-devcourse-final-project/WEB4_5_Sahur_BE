cfcce192ad279a0874aafc22fde7072923310315
jxxngin <wwd0203@daum.net>
2025-05-01 12:28:08 +0900

    feat: 주문 조회 최적화를 위한 EntityGraph 적용

--- backend/src/main/java/com/team5/backend/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/team5/backend/domain/order/repository/OrderRepository.java
@@ -1,8 +1,18 @@
 package com.team5.backend.domain.order.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team5.backend.domain.order.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+	@EntityGraph(attributePaths = {"member", "groupBuy", "groupBuy.product"})
+	Page<Order> findAll(Pageable pageable);
+
+	@EntityGraph(attributePaths = {"member", "groupBuy", "groupBuy.product"})
+	Optional<Order> findWithDetailsById(Long orderId);
 }
