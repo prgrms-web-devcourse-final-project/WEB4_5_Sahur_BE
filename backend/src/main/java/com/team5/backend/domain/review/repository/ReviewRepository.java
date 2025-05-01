74671beed3a297f7c3f94ae5a9d204d6e9c0b2e2
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 09:44:33 +0900

    feat: 리뷰 리포지토리 멤버Id 혹은 상품Id로 리뷰 조회 구현

--- backend/src/main/java/com/team5/backend/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/team5/backend/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.team5.backend.domain.review.repository;
 
 import com.team5.backend.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findByProduct_ProductId(Long productId, Pageable pageable);
+    Page<Review> findByMember_MemberId(Long memberId, Pageable pageable);
 }
