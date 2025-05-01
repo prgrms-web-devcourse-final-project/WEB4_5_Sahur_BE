4aade4a2b2d4dac421cbf68e64b70d81be11ef00
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 11:12:55 +0900

    refactor: 알림관련 요청 dto 테스트 코드 작성을 위해 @Builder 추가

--- backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationCreateReqDto.java
@@ -1,9 +1,11 @@
 package com.team5.backend.domain.notification.dto;
 
 import com.team5.backend.domain.notification.entity.NotificationType;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class NotificationCreateReqDto {
     private Long memberId;
     private NotificationType type;
--- backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationUpdateReqDto.java
@@ -1,9 +1,11 @@
 package com.team5.backend.domain.notification.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
+@Builder
 public class NotificationUpdateReqDto {
     private Boolean read;
 }
