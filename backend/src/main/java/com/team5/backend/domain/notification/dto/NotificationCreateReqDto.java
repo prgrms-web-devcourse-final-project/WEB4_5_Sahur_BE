54653f8e711e1ce11c7fc3fed997af99d575c935
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 11:25:16 +0900

    refactor: 알림 생성, 수정 Dto @NotNull 추가

--- backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationCreateReqDto.java
@@ -1,15 +1,21 @@
 package com.team5.backend.domain.notification.dto;
 
 import com.team5.backend.domain.notification.entity.NotificationType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class NotificationCreateReqDto {
+    @NotNull
     private Long memberId;
+    @NotNull
     private NotificationType type;
+    @NotNull
     private String title;
+    @NotNull
     private String message;
+    @NotNull
     private String url;
 }
--- backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/notification/dto/NotificationUpdateReqDto.java
@@ -1,5 +1,7 @@
 package com.team5.backend.domain.notification.dto;
 
+import com.team5.backend.domain.notification.entity.NotificationType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,5 +9,14 @@ import lombok.Setter;
 @Getter
 @Builder
 public class NotificationUpdateReqDto {
+    @NotNull
+    private NotificationType type;
+    @NotNull
+    private String title;
+    @NotNull
+    private String message;
+    @NotNull
+    private String url;
+    @NotNull
     private Boolean read;
 }
