74724bb40119fdd1b314e6764ee5b1352a3fa491
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 10:06:34 +0900

    refactor: 리뷰 updateReqDto에 @NotNull추가

--- backend/src/main/java/com/team5/backend/domain/review/dto/ReviewUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/review/dto/ReviewUpdateReqDto.java
@@ -1,12 +1,16 @@
 package com.team5.backend.domain.review.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ReviewUpdateReqDto {
+    @NotNull
     private String comment;
+    @NotNull
     private Integer rate;
+    @NotNull
     private String imageUrl;
 }
