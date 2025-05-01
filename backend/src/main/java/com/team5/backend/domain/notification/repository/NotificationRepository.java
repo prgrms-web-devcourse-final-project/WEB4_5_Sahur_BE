6991ec8e5f55fe16d907c702a1219ba366e7addc
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 11:44:27 +0900

    feat: 알림리포지토리 멤버Id로 멤버의 알림 조회 기능 구현

--- backend/src/main/java/com/team5/backend/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/team5/backend/domain/notification/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.team5.backend.domain.notification.repository;
 
 import com.team5.backend.domain.notification.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Page<Notification> findByMemberId(Long memberId, Pageable pageable);
 }
