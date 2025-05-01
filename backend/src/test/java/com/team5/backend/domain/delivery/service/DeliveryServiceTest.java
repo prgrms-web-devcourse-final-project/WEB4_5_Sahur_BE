1a9ffcf96f4420b88d0515f0776ee908fcced905
yunjuKimm <yunju.tech@gmail.com>
2025-05-01 17:26:47 +0900

    test: DeliveryService 테스트 수정

--- backend/src/test/java/com/team5/backend/domain/delivery/service/DeliveryServiceTest.java
+++ backend/src/test/java/com/team5/backend/domain/delivery/service/DeliveryServiceTest.java
@@ -2,6 +2,7 @@ package com.team5.backend.domain.delivery.service;
 
 import com.team5.backend.domain.delivery.dto.DeliveryReqDto;
 import com.team5.backend.domain.delivery.entity.Delivery;
+import com.team5.backend.domain.delivery.entity.DeliveryStatus;
 import com.team5.backend.domain.delivery.repository.DeliveryRepository;
 import com.team5.backend.domain.order.entity.Order;
 import com.team5.backend.domain.order.repository.OrderRepository;
@@ -16,11 +17,11 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -40,7 +41,13 @@ class DeliveryServiceTest {
     void create() {
         // given
         Long orderId = 1L;
-        DeliveryReqDto request = new DeliveryReqDto("서울시 어쩌구", "01012345678", 12345);
+        DeliveryReqDto request = new DeliveryReqDto(
+                "서울시 어쩌구",
+                12345,
+                "01012345678",
+                DeliveryStatus.PREPARING,
+                "12345"
+        );
         Order order = Order.builder().orderId(orderId).build();
 
         given(orderRepository.findById(orderId)).willReturn(Optional.of(order));
@@ -53,7 +60,10 @@ class DeliveryServiceTest {
         assertThat(result.getAddress()).isEqualTo("서울시 어쩌구");
         assertThat(result.getContact()).isEqualTo("01012345678");
         assertThat(result.getPccc()).isEqualTo(12345);
+        assertThat(result.getStatus()).isEqualTo(DeliveryStatus.PREPARING);
+        assertThat(result.getShipping()).isEqualTo("12345");
     }
+
 
     @DisplayName("배송 단건 조회 성공")
     @Test
@@ -61,7 +71,15 @@ class DeliveryServiceTest {
         // given
         Long orderId = 2L;
         Order mockOrder = Order.builder().orderId(orderId).build();
-        Delivery delivery = Delivery.create(mockOrder, "서울시 어쩌구", "01012345678",12345);
+
+        Delivery delivery = Delivery.create(
+                mockOrder,
+                "서울시 어쩌구",
+                12345,
+                "01012345678",
+                DeliveryStatus.PREPARING,
+                "12345"
+        );
 
         given(deliveryRepository.findByOrderOrderId(orderId)).willReturn(Optional.of(delivery));
 
@@ -70,8 +88,10 @@ class DeliveryServiceTest {
 
         // then
         assertThat(result.getAddress()).isEqualTo("서울시 어쩌구");
-        assertThat(result.getContact()).isEqualTo("01012345678");
         assertThat(result.getPccc()).isEqualTo(12345);
+        assertThat(result.getContact()).isEqualTo("01012345678");
+        assertThat(result.getStatus()).isEqualTo(DeliveryStatus.PREPARING);
+        assertThat(result.getShipping()).isEqualTo("12345");
     }
 
 
@@ -80,9 +100,23 @@ class DeliveryServiceTest {
     void update() {
         // given
         Long deliveryId = 3L;
-        DeliveryReqDto request = new DeliveryReqDto("서울시 00구", "01012345678", 77777);
+        DeliveryReqDto request = new DeliveryReqDto(
+                "서울시 00구",
+                77777,
+                "01012345678",
+                DeliveryStatus.COMPLETED,
+                "98765"
+        );
         Order mockOrder = Order.builder().orderId(100L).build();
-        Delivery delivery = Delivery.create(mockOrder, "기존주소", "01011223344", 12345);
+
+        Delivery delivery = Delivery.create(
+                mockOrder,
+                "기존주소",
+                12345,
+                "01011223344",
+                DeliveryStatus.PREPARING,
+                "11111"
+        );
 
         given(deliveryRepository.findById(deliveryId)).willReturn(Optional.of(delivery));
 
@@ -91,8 +125,10 @@ class DeliveryServiceTest {
 
         // then
         assertThat(result.getAddress()).isEqualTo("서울시 00구");
-        assertThat(result.getContact()).isEqualTo("01012345678");
         assertThat(result.getPccc()).isEqualTo(77777);
+        assertThat(result.getContact()).isEqualTo("01012345678");
+        assertThat(result.getStatus()).isEqualTo(DeliveryStatus.COMPLETED);
+        assertThat(result.getShipping()).isEqualTo("98765");
     }
 
     @DisplayName("배송 삭제 성공")
@@ -116,8 +152,8 @@ class DeliveryServiceTest {
     void getAll() {
         // given
         List<Delivery> list = List.of(
-                Delivery.create(null, "서울시 강남구", "01011112222", 11111),
-                Delivery.create(null, "서울시 종로구", "01033334444", 22222)
+                Delivery.create(null, "서울시 강남구", 11111, "01011112222", DeliveryStatus.PREPARING, "0001"),
+                Delivery.create(null, "서울시 종로구", 22222, "01033334444", DeliveryStatus.INDELIVERY, "0002")
         );
 
         given(deliveryRepository.findAll()).willReturn(list);
@@ -128,7 +164,16 @@ class DeliveryServiceTest {
         // then
         assertThat(result).hasSize(2);
         assertThat(result.get(0).getAddress()).isEqualTo("서울시 강남구");
+        assertThat(result.get(0).getPccc()).isEqualTo(11111);
+        assertThat(result.get(0).getContact()).isEqualTo("01011112222");
+        assertThat(result.get(0).getStatus()).isEqualTo(DeliveryStatus.PREPARING);
+        assertThat(result.get(0).getShipping()).isEqualTo("0001");
+
         assertThat(result.get(1).getAddress()).isEqualTo("서울시 종로구");
+        assertThat(result.get(1).getPccc()).isEqualTo(22222);
+        assertThat(result.get(1).getContact()).isEqualTo("01033334444");
+        assertThat(result.get(1).getStatus()).isEqualTo(DeliveryStatus.INDELIVERY);
+        assertThat(result.get(1).getShipping()).isEqualTo("0002");
     }
 
 
@@ -139,10 +184,15 @@ class DeliveryServiceTest {
         Long orderId = 999L;
         given(orderRepository.findById(orderId)).willReturn(Optional.empty());
 
+        DeliveryReqDto request = new DeliveryReqDto(
+                "서울시 00구",
+                12345,
+                "01000000000",
+                DeliveryStatus.PREPARING,
+                "0001"
+        );
         // when & then
-        assertThatThrownBy(() ->
-                deliveryService.createDelivery(orderId, new DeliveryReqDto("a", "b", 1))
-        )
+        assertThatThrownBy(() -> deliveryService.createDelivery(orderId, request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("주문을 찾을 수 없습니다");
     }
@@ -153,7 +203,13 @@ class DeliveryServiceTest {
     void updateFail_DeliveryNotFound() {
         // given
         Long deliveryId = 100L;
-        DeliveryReqDto request = new DeliveryReqDto("부산시 00구", "01099998888", 55555);
+        DeliveryReqDto request = new DeliveryReqDto(
+                "부산시 00구",
+                55555,
+                "01099998888",
+                DeliveryStatus.INDELIVERY,
+                "12121"
+        );
         given(deliveryRepository.findById(deliveryId)).willReturn(Optional.empty());
 
         // when & then
