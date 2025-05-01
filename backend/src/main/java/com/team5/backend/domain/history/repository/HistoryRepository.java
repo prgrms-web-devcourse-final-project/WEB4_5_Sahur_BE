03eb16bac6fa2e9fe2fbf3e4b770797d14092a41
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-01 14:49:26 +0900

    feat: 구매내역리포지토리 회원id,멤버 id로 리뷰 작성 가능한/불가한 구매내역들 찾기

--- backend/src/main/java/com/team5/backend/domain/history/repository/HistoryRepository.java
+++ backend/src/main/java/com/team5/backend/domain/history/repository/HistoryRepository.java
@@ -9,10 +9,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
     @Query("SELECT DISTINCT h.groupBuy FROM History h WHERE h.member.memberId = :memberId")
     Page<GroupBuy> findDistinctGroupBuysByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+
+    // writable = true 인 모든 기록
+    List<History> findAllByMemberIdAndProductIdAndWritableTrue(Long memberId, Long productId);
+
+    // writable = false 인 모든 기록
+    List<History> findAllByMemberIdAndProductIdAndWritableFalse(Long memberId, Long productId);
+
 
 
 }
