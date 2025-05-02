1d9108eff3f3f254e2f3d327645bf3738489ce33
yunjuKimm <yunju.tech@gmail.com>
2025-05-02 09:35:26 +0900

    refactor: Dibs Controller api 명세서에 맞게 수정

--- backend/src/main/java/com/team5/backend/domain/dibs/controller/DibsController.java
+++ backend/src/main/java/com/team5/backend/domain/dibs/controller/DibsController.java
@@ -3,8 +3,10 @@ package com.team5.backend.domain.dibs.controller;
 import com.team5.backend.domain.dibs.dto.DibsCreateReqDto;
 import com.team5.backend.domain.dibs.dto.DibsResDto;
 import com.team5.backend.domain.dibs.service.DibsService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +19,41 @@ public class DibsController {
 
     private final DibsService dibsService;
 
-    @PostMapping
-    public ResponseEntity<DibsResDto> createDibs(@RequestBody @Valid DibsCreateReqDto request) {
+    @PostMapping("/products/{productId}/dibs")
+    public ResponseEntity<DibsResDto> createDibs(
+            @PathVariable Long productId,
+            @RequestParam Long memberId){
+        DibsCreateReqDto request = new DibsCreateReqDto(memberId, productId);
         DibsResDto response = dibsService.createDibs(request);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
-    public ResponseEntity<List<DibsResDto>> getAllDibs() {
-        List<DibsResDto> responses = dibsService.getAllDibs();
-        return ResponseEntity.ok(responses);
+//    @GetMapping("/members/{memberId}/dibs")
+//    public ResponseEntity<List<DibsResDto>> getAllDibs() {
+//        List<DibsResDto> responses = dibsService.getAllDibs();
+//        return ResponseEntity.ok(responses);
+//    }
+
+    @DeleteMapping("/products/{productId}/dibs")
+    public ResponseEntity<Void> deleteDibs(
+            @PathVariable Long productId,
+            @RequestParam Long memberId){
+        dibsService.deleteByProductAndMember(productId, memberId);
+        return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{dibsId}")
-    public ResponseEntity<Void> deleteDibs(@PathVariable Long dibsId) {
-        dibsService.deleteDibs(dibsId);
-        return ResponseEntity.noContent().build();
+    @GetMapping("/members/{memberId}/dibs")
+    public ResponseEntity<?> getDibsByMember(
+            @PathVariable Long memberId,
+            @PageableDefault(size = 6) Pageable pageable,
+            @RequestParam(required = false) Boolean paged) {
+
+        if (Boolean.TRUE.equals(paged)) {
+            Page<DibsResDto> pagedDibs = dibsService.getPagedDibsByMemberId(memberId, pageable);
+            return ResponseEntity.ok(pagedDibs);
+        }
+
+        List<DibsResDto> all = dibsService.getAllDibsByMemberId(memberId);
+        return ResponseEntity.ok(all);
     }
 }
