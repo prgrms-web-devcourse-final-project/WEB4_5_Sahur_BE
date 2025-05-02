8ee6e9f78e80e7c19c115f187d673aded9e83f19
Jeongin <89836139+jxxngin@users.noreply.github.com>
2025-05-02 11:31:15 +0900

    Test/#58 결제 테스트 코드 작성 (#74)
    * test: PaymentService 주요 기능 단위 테스트 작성
    
    * test: TossService 결제 승인/조회/취소 로직 테스트 추가
    
    * chore: 테스트 편의를 위한 AllArgsConstructor 추가
    
    * test: PaymentController 전체 API 단위 테스트 작성
    
    * fix: Long 타입 필드 orderId에 맞게 @NotBlank → @NotNull 적용
    
    * test: 실패 케이스 테스트를 위해 RsData 오류 응답 분기 임시 추가
    
    * Sytle: 테스트 출력 System.out.println 제거

--- backend/src/main/java/com/team5/backend/domain/payment/dto/CancelReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/payment/dto/CancelReqDto.java
@@ -1,9 +1,13 @@
 package com.team5.backend.domain.payment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CancelReqDto {
 	@NotBlank(message = "취소 사유는 필수입니다.")
 	private String cancelReason;
