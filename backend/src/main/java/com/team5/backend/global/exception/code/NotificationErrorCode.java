7cf5d35917f6147724d30b9d2d65a122e3b7f77c
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-04 21:32:39 +0900

    refactor: 알림커스텀 예외코드 토큰 관련 커스텀예외 추가

--- backend/src/main/java/com/team5/backend/global/exception/code/NotificationErrorCode.java
+++ backend/src/main/java/com/team5/backend/global/exception/code/NotificationErrorCode.java
@@ -4,7 +4,9 @@ import com.team5.backend.global.exception.ErrorCode;
 
 public enum NotificationErrorCode implements ErrorCode {
     MEMBER_NOT_FOUND(404, "MEMBER_NOT_FOUND", "회원을 찾을 수 없습니다."),
-    NOTIFICATION_NOT_FOUND(404, "NOTIFICATION_NOT_FOUND", "알림을 찾을 수 없습니다.");
+    NOTIFICATION_NOT_FOUND(404, "NOTIFICATION_NOT_FOUND", "알림을 찾을 수 없습니다."),
+    TOKEN_INVALID(401, "NOTIFICATION_TOKEN_INVALID", "유효하지 않은 토큰입니다."),
+    TOKEN_BLACKLISTED(401, "NOTIFICATION_TOKEN_BLACKLISTED", "로그아웃된 토큰입니다.");
 
     private final int status;
     private final String code;
