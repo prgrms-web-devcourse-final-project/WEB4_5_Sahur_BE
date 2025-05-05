1a89a61ed1af0b950a5fc1f304b47b4d17ba0ddf
Jeongin <89836139+jxxngin@users.noreply.github.com>
2025-05-05 23:22:39 +0900

    Feat/#89 주문 조회 api 추가 구현 (#90)
    * refactor: 불필요한 Service 의존성 제거
    
    * feat: 주문 목록 조회 API에 주문번호 및 상태 필터링 기능 추가
    
    * feat: 주문 목록 필터링 기능 테스트 코드 반영
    
    * feat: 회원별 주문 조회 기능 구현 및 상태별 필터링 지원
    
    * test: 회원 주문 조회 기능 테스트 케이스 작성
    
    * refactor: 주문 취소 응답 RsData 포맷으로 변경
    
    * test: 주문 취소 API 응답 포맷에 맞춰 테스트 코드 수정
    
    * docs: Swagger 문서화 - OrderController에 Tag 및 각 API 설명 추가
    
    * merge: main branch
    
    * refactor: Order 상태 및 회원 기준 페이징 쿼리 메서드 정리
    
    * refactor: @Transactional 추가
    
    * test: BaseInitData 기반 OrderControllerTest 리팩토링
    
    * test: BaseInitData 기반 OrderServiceTest 리팩토링

--- backend/src/main/java/com/team5/backend/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/team5/backend/domain/order/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.team5.backend.domain.order.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -8,10 +9,22 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team5.backend.domain.order.entity.Order;
+import com.team5.backend.domain.order.entity.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	@EntityGraph(attributePaths = {"member", "groupBuy", "product"})
 	Page<Order> findAll(Pageable pageable);
+
+	@EntityGraph(attributePaths = {"member", "groupBuy", "product"})
+	Page<Order> findByOrderId(Long orderId, Pageable pageable);
+
+	Page<Order> findByStatus(OrderStatus status, Pageable pageable);
+
+	@EntityGraph(attributePaths = {"member", "product", "groupBuy"})
+	Page<Order> findByMember_MemberId(Long memberId, Pageable pageable);
+
+	@EntityGraph(attributePaths = {"member", "product", "groupBuy"})
+	Page<Order> findByMember_MemberIdAndStatusIn(Long memberId, List<OrderStatus> status, Pageable pageable);
 
 	@EntityGraph(attributePaths = {"member", "groupBuy", "product"})
 	Optional<Order> findWithDetailsByOrderId(Long orderId);
