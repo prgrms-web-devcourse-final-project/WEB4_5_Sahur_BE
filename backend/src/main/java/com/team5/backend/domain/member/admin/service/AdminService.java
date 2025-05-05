acfdcf5b60199888d2baa80dbbdb0e1e78b58ae9
yunjuKimm <yunju.tech@gmail.com>
2025-05-05 18:59:32 +0900

    feat: 관리자 공동구매 생성 기능 구현, 예외처리 구현

--- backend/src/main/java/com/team5/backend/domain/member/admin/service/AdminService.java
+++ backend/src/main/java/com/team5/backend/domain/member/admin/service/AdminService.java
@@ -1,21 +1,34 @@
 package com.team5.backend.domain.member.admin.service;
 
+import com.team5.backend.domain.groupBuy.dto.GroupBuyCreateReqDto;
+import com.team5.backend.domain.groupBuy.entity.GroupBuy;
+import com.team5.backend.domain.groupBuy.entity.GroupBuyStatus;
+import com.team5.backend.domain.groupBuy.repository.GroupBuyRepository;
 import com.team5.backend.domain.member.admin.dto.ProductRequestResDto;
 import com.team5.backend.domain.member.admin.entity.ProductRequest;
 import com.team5.backend.domain.member.admin.entity.ProductRequestStatus;
 import com.team5.backend.domain.member.admin.repository.ProductRequestRepository;
+import com.team5.backend.domain.product.entity.Product;
+import com.team5.backend.domain.product.repository.ProductRepository;
+import com.team5.backend.global.exception.CustomException;
+import com.team5.backend.global.exception.code.AdminErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
     private final ProductRequestRepository productRequestRepository;
+    private final ProductRepository productRepository;
+    private final GroupBuyRepository groupBuyRepository;
 
     public Page<ProductRequestResDto> getProductRequests(Pageable pageable, ProductRequestStatus status) {
         Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
@@ -27,5 +40,24 @@ public class AdminService {
 
         return pageResult.map(ProductRequestResDto::fromEntity);
     }
+
+    @Transactional
+    public void createGroupBuy(GroupBuyCreateReqDto request) {
+        Product product = productRepository.findById(request.getProductId())
+                .orElseThrow(() -> new CustomException(AdminErrorCode.PRODUCT_NOT_FOUND));
+
+        GroupBuy groupBuy = GroupBuy.builder()
+                .product(product)
+                .targetParticipants(request.getTargetParticipants())
+                .currentParticipantCount(0)
+                .round(request.getRound())
+                .deadline(request.getDeadline())
+                .status(GroupBuyStatus.ONGOING)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        groupBuyRepository.save(groupBuy);
+    }
+
 
 }
