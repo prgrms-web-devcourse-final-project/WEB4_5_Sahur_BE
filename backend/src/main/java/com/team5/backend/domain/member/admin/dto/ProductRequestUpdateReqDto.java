8f1bf32598273fdecc09e21036eddd42c91a2def
yunjuKimm <yunju.tech@gmail.com>
2025-05-05 16:51:28 +0900

    feat: admin 상품등록요청 상태 수정 및 공동구매 생성 api 구현

--- backend/src/main/java/com/team5/backend/domain/member/admin/dto/ProductRequestUpdateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/member/admin/dto/ProductRequestUpdateReqDto.java
@@ -1,8 +1,10 @@
 import com.team5.backend.domain.member.admin.entity.ProductRequestStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ProductRequestUpdateReqDto {
     @NotNull
     private ProductRequestStatus status;
