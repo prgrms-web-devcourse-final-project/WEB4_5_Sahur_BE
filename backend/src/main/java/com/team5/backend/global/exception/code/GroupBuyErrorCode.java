79823bc6a348a307d4a9df626d986edffa9c7494
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-05 23:50:23 +0900

    feat: 공동구매 커스텀 예외처리 이미 마감된 공동구매 추가

--- backend/src/main/java/com/team5/backend/global/exception/code/GroupBuyErrorCode.java
+++ backend/src/main/java/com/team5/backend/global/exception/code/GroupBuyErrorCode.java
@@ -7,7 +7,8 @@ public enum GroupBuyErrorCode implements ErrorCode {
     PRODUCT_NOT_FOUND(404, "PRODUCT_NOT_FOUND", "해당 상품을 찾을 수 없습니다."),
     CATEGORY_NOT_FOUND(404, "CATEGORY_NOT_FOUND", "해당 카테고리를 찾을 수 없습니다."),
     TOKEN_INVALID(401, "TOKEN_INVALID", "유효하지 않은 토큰입니다."),
-    TOKEN_BLACKLISTED(401, "TOKEN_BLACKLISTED", "로그아웃된 토큰입니다.");
+    TOKEN_BLACKLISTED(401, "TOKEN_BLACKLISTED", "로그아웃된 토큰입니다."),
+    GROUP_BUY_ALREADY_CLOSED(401, "ALREADY_CLOSED" ,"이미 종료된 공동구매입니다." );
 
     private final int status;
     private final String code;
