512449d7ee671443857db753216988560dc7bcf4
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-06 22:39:49 +0900

    feat: 공동구매 상품의 평균 평점과 dib여부를 포함한 detailResDto 구현

---
+++ backend/src/main/java/com/team5/backend/domain/groupBuy/dto/GroupBuyDetailResDto.java
@@ -0,0 +1,40 @@
+package com.team5.backend.domain.groupBuy.dto;
+
+import com.team5.backend.domain.groupBuy.entity.GroupBuy;
+import com.team5.backend.domain.groupBuy.entity.GroupBuyStatus;
+import com.team5.backend.domain.product.entity.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class GroupBuyDetailResDto {
+    private Long groupBuyId;
+    private Product product;
+    private Double averageRate;
+    private Integer targetParticipants;
+    private Integer currentParticipantCount;
+    private Integer round;
+    private LocalDateTime deadline;
+    private GroupBuyStatus status;
+    private boolean isDeadlineToday;
+    private boolean isDibs;
+
+    public static GroupBuyDetailResDto fromEntity(GroupBuy groupBuy, boolean isTodayDeadline, boolean isDibs, Double averageRate) {
+
+        return GroupBuyDetailResDto.builder()
+                .groupBuyId(groupBuy.getGroupBuyId())
+                .product(groupBuy.getProduct())
+                .averageRate(averageRate)
+                .targetParticipants(groupBuy.getTargetParticipants())
+                .currentParticipantCount(groupBuy.getCurrentParticipantCount())
+                .round(groupBuy.getRound())
+                .deadline(groupBuy.getDeadline())
+                .status(groupBuy.getStatus())
+                .isDeadlineToday(isTodayDeadline)
+                .isDibs(isDibs)
+                .build();
+    }
+}
