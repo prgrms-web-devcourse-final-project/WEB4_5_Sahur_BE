b975e4786a10414b1f8a9e4bb3f4fe259648661f
linedj <eunjun998@gmail.com>
2025-05-13 22:26:22 +0900

    feat: elasticsearch

--- backend/src/main/java/com/team5/backend/domain/product/search/repository/ProductSearchRepository.java
+++ backend/src/main/java/com/team5/backend/domain/product/search/repository/ProductSearchRepository.java
@@ -1,11 +1,11 @@
-//package com.team5.backend.domain.product.search.repository;
-//
-//import com.team5.backend.domain.product.search.document.ProductDocument;
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//
-//import java.util.List;
-//
-//public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
-//    List<ProductDocument> findByTitleContainingOrDescriptionContaining(String title, String description);
-//}
-//
+package com.team5.backend.domain.product.search.repository;
+
+import com.team5.backend.domain.product.search.document.ProductDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
+    List<ProductDocument> findByTitleContainingOrDescriptionContaining(String title, String description);
+}
+
