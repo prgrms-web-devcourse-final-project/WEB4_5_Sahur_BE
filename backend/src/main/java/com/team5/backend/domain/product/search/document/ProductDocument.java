ce52f6a71022d937ca3226141ca88037de7d0c05
linedj <eunjun998@gmail.com>
2025-05-13 18:56:32 +0900

    fix: annotation

--- backend/src/main/java/com/team5/backend/domain/product/search/document/ProductDocument.java
+++ backend/src/main/java/com/team5/backend/domain/product/search/document/ProductDocument.java
@@ -1,28 +1,28 @@
-package com.team5.backend.domain.product.search.document;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Id;
-import lombok.*;
-import org.springframework.data.elasticsearch.annotations.Document;
-
-import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Document(indexName = "products")
-@Data
-@Builder
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductDocument {
-    @Id
-    private String id;
-
-    private Long categoryId;
-    private String title;
-    private String description;
-    private List<String> imageUrl;
-    private Integer price;
-    private Long dibCount;
-    private String createdAt;
-}
+//package com.team5.backend.domain.product.search.document;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import jakarta.persistence.Id;
+//import lombok.*;
+//import org.springframework.data.elasticsearch.annotations.Document;
+//
+//import java.util.List;
+//
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//@Document(indexName = "products")
+//@Data
+//@Builder
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class ProductDocument {
+//    @Id
+//    private String id;
+//
+//    private Long categoryId;
+//    private String title;
+//    private String description;
+//    private List<String> imageUrl;
+//    private Integer price;
+//    private Long dibCount;
+//    private String createdAt;
+//}
