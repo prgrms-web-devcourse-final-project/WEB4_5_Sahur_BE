7ef9b88017be641420b8db687122fc08588621d7
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-14 14:35:01 +0900

    Revert "style: category -> categoryType으로 수정"
    This reverts commit 773b8aba0cfdc9cae1320666c2f9d1713027b840.

--- backend/src/main/java/com/team5/backend/domain/category/dto/CategoryDto.java
+++ backend/src/main/java/com/team5/backend/domain/category/dto/CategoryDto.java
@@ -13,14 +13,14 @@ import lombok.Getter;
 @Builder
 public class CategoryDto {
     private Long categoryId;
-    private CategoryType categoryType;
+    private CategoryType category;
     private KeywordType keyword;
     private Integer uid;
 
     public static CategoryDto fromEntity(Category category) {
         return CategoryDto.builder()
                 .categoryId(category.getCategoryId())
-                .categoryType(category.getCategoryType())
+                .category(category.getCategory())
                 .keyword(category.getKeyword())
                 .uid(category.getUid())
                 .build();
