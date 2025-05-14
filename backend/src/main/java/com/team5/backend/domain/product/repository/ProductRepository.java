27ffdbacd9db488fd4ea0f0e8a6210c6c8228d6b
yunjuKimm <yunju.tech@gmail.com>
2025-05-14 14:04:58 +0900

    refactor: ProductRepository 쿼리 CategoryType, KeywordType 변경

--- backend/src/main/java/com/team5/backend/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/com/team5/backend/domain/product/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.team5.backend.domain.product.repository;
 
+import com.team5.backend.domain.category.entity.CategoryType;
+import com.team5.backend.domain.category.entity.KeywordType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +11,6 @@ import com.team5.backend.domain.product.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom  {
-	Page<Product> findByCategory_Category(String category, Pageable pageable);	// 카테고리 필드 기준 검색
-	Page<Product> findByCategory_Keyword(String keyword, Pageable pageable);	// 키워드 필드 기준 검색
+	Page<Product> findByCategory_CategoryType(CategoryType categoryType, Pageable pageable);	// 카테고리 필드 기준 검색
+	Page<Product> findByCategory_Keyword(KeywordType keyword, Pageable pageable);	// 키워드 필드 기준 검색
 }
