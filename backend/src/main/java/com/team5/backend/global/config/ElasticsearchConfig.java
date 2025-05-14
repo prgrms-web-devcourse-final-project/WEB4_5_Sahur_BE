ec2868185c876477f1a2f7cd8fdbe739240f7888
linedj <eunjun998@gmail.com>
2025-05-14 11:46:38 +0900

    fix: path

--- backend/src/main/java/com/team5/backend/global/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/team5/backend/global/config/ElasticsearchConfig.java
@@ -1,40 +1,31 @@
 package com.team5.backend.global.config;
 
-//@Configuration
-//public class ElasticsearchConfig {
-//
-//    @Value("${custom.db.host}")
-//    private String elasticsearchHost;
-//
-//    @Bean
-//    public ElasticsearchClient elasticsearchClient() {
-//        RestClient restClient = RestClient.builder(
-//                new HttpHost(elasticsearchHost, 9200, "http")
-//        ).build();
-//
-//        ElasticsearchTransport transport = new RestClientTransport(
-//                restClient, new JacksonJsonpMapper()
-//        );
-//
-//        return new ElasticsearchClient(transport);
-//    }
-//}
-
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
+public class ElasticsearchConfig {
 
     @Value("${custom.elastic.host}")
     private String elasticsearchHost;
 
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(elasticsearchHost + ":9200")
-                .build();
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost(elasticsearchHost, 9200, "http")
+        ).build();
+
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper()
+        );
+
+        return new ElasticsearchClient(transport);
     }
 }
