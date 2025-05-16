22f47e1f918a2ff2e12a206a11a06ba7da9d2aba
SDHSeoulTech <113351553+SDHSeoulTech@users.noreply.github.com>
2025-05-16 10:20:38 +0900

    Feat/#179 공동구매 카테고리 검색 api 구현 (#180)
    * feat: 공동구매 리포지토리 카테고리로 공동구매 조회
    
    * feat: 공동구매 서비스 카테고리로 공동구매 조회 메서드 구현
    
    * feat: 공동구매 컨트롤러 카테고리로 공동구매 조회 api 구현
    
    * feat: 공동구매 단건조회 반환 dto에 총 리뷰 갯수 추가
    
    * feat: 리뷰 리포지토리에 상품 별 리뷰 갯수 반환 메서드 추가
    
    * refactor: 공동구매서비스 단건조회 lazy 방지
    
    * refactor: GroupBuyViewAspect Lazy 방지
    
    * feat: 공동구매 리포지토리 공동구매 단건 조회 메서드 수정

--- backend/src/main/java/com/team5/backend/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/team5/backend/domain/review/repository/ReviewRepository.java
@@ -12,5 +12,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByMemberMemberId(Long memberId, Pageable pageable);
     @Query("SELECT AVG(r.rate) FROM Review r WHERE r.product.productId = :productId")
     Double findAverageRatingByProductId(@Param("productId") Long productId);
+    @Query("SELECT COUNT(r) FROM Review r WHERE r.product.productId = :productId")
+    Integer countByProductId(@Param("productId") Long productId);
+
 
 }
