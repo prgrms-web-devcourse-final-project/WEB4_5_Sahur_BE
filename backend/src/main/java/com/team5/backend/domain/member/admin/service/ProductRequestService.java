8a616fd9f337698e11a7ebea9ca0d9d288007ef4
yunjuKimm <yunju.tech@gmail.com>
2025-05-18 21:24:34 +0900

    feat: 상품 등록 요청 서비스 구현

---
+++ backend/src/main/java/com/team5/backend/domain/member/admin/service/ProductRequestService.java
@@ -0,0 +1,50 @@
+package com.team5.backend.domain.member.admin.service;
+
+import com.team5.backend.domain.category.entity.Category;
+import com.team5.backend.domain.category.repository.CategoryRepository;
+import com.team5.backend.domain.member.admin.dto.ProductRequestCreateReqDto;
+import com.team5.backend.domain.member.admin.dto.ProductRequestResDto;
+import com.team5.backend.domain.member.admin.entity.ProductRequest;
+import com.team5.backend.domain.member.admin.entity.ProductRequestStatus;
+import com.team5.backend.domain.member.admin.repository.ProductRequestRepository;
+import com.team5.backend.domain.member.member.entity.Member;
+import com.team5.backend.domain.member.member.repository.MemberRepository;
+import com.team5.backend.global.exception.CustomException;
+import com.team5.backend.global.exception.code.MemberErrorCode;
+import com.team5.backend.global.exception.code.ProductErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductRequestService {
+
+    private final MemberRepository memberRepository;
+    private final ProductRequestRepository productRequestRepository;
+    private final CategoryRepository categoryRepository;
+
+
+
+    @Transactional
+    public ProductRequestResDto createRequest(Long memberId, ProductRequestCreateReqDto requestDto) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        Category category = categoryRepository.findById(requestDto.getCategoryId())
+                .orElseThrow(() -> new CustomException(ProductErrorCode.CATEGORY_NOT_FOUND));
+
+        ProductRequest productRequest = ProductRequest.builder()
+                .member(member)
+                .category(category)
+                .title(requestDto.getTitle())
+                .productUrl(requestDto.getProductUrl())
+                .imageUrls(requestDto.getImageUrls())
+                .description(requestDto.getDescription())
+                .status(ProductRequestStatus.WAITING)
+                .build();
+        ProductRequest savedProductRequest  = productRequestRepository.save(productRequest);
+        return ProductRequestResDto.fromEntity(savedProductRequest );
+    }
+
+}
