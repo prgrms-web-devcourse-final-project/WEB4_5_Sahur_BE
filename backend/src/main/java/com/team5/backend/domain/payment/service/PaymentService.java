d885c440d30d21addc7cf5f5c8a254485aa25ed7
SDHSeoulTech <dongseoultech@gmail.com>
2025-05-20 10:57:59 +0900

    refactor: 결제 완료시 자동으로 구매내역 및 알림 생성

--- backend/src/main/java/com/team5/backend/domain/payment/service/PaymentService.java
+++ backend/src/main/java/com/team5/backend/domain/payment/service/PaymentService.java
@@ -3,6 +3,11 @@ package com.team5.backend.domain.payment.service;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.team5.backend.domain.history.entity.History;
+import com.team5.backend.domain.history.repository.HistoryRepository;
+import com.team5.backend.domain.notification.entity.Notification;
+import com.team5.backend.domain.notification.entity.NotificationType;
+import com.team5.backend.domain.notification.repository.NotificationRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -29,18 +34,43 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
     private final TossService tossService;
+    private final HistoryRepository historyRepository;
+    private final NotificationRepository notificationRepository;
 
+    @Transactional
     public void savePayment(Long orderId, String paymentKey) {
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new CustomException(PaymentErrorCode.ORDER_NOT_FOUND));
 
-        // 주문 상태를 PAID 변경
+        // 주문 상태를 PAID로 변경
         order.markAsPaid();
 
         // 결제 엔티티 저장
         Payment payment = Payment.create(order, paymentKey);
         paymentRepository.save(payment);
+
+        // History 생성
+        History history = History.builder()
+                .member(order.getMember())
+                .product(order.getProduct())
+                .groupBuy(order.getGroupBuy())
+                .order(order)
+                .writable(true)
+                .build();
+        historyRepository.save(history);
+
+        // Notification 생성
+        Notification notification = Notification.builder()
+                .member(order.getMember())
+                .type(NotificationType.ORDER)
+                .title("구매 완료 알림")
+                .message("[" + order.getProduct().getTitle() + "] 상품의 구매가 완료되었습니다.")
+                .url("/orders/" + order.getOrderId()) // 예시 URL
+                .isRead(false)
+                .build();
+        notificationRepository.save(notification);
     }
+
 
     public String getPaymentKey(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId)
