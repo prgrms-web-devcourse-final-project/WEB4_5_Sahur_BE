905d2da25b1c03f82e8f172578bc7a379570dc38
kozae00 <rhwogus777@gmail.com>
2025-05-23 16:38:27 +0900

    refactor: resDto 수정

--- backend/src/main/java/com/team5/backend/domain/dibs/repository/DibsRepository.java
+++ backend/src/main/java/com/team5/backend/domain/dibs/repository/DibsRepository.java
@@ -1,9 +1,11 @@
 package com.team5.backend.domain.dibs.repository;
 
 import com.team5.backend.domain.dibs.entity.Dibs;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,5 +19,13 @@ public interface DibsRepository extends JpaRepository<Dibs, Long> {
     List<Dibs> findByMember_MemberId(Long memberId);
 
     Optional<Dibs> findByProduct_ProductIdAndMember_MemberId(Long productId, Long memberId);
+
+    @Query("SELECT d FROM Dibs d JOIN FETCH d.product WHERE d.member.memberId = :memberId")
+    List<Dibs> findAllWithProductByMemberId(@Param("memberId") Long memberId);
+
+    @Query(value = "SELECT d FROM Dibs d JOIN FETCH d.product WHERE d.member.memberId = :memberId",
+            countQuery = "SELECT COUNT(d) FROM Dibs d WHERE d.member.memberId = :memberId")
+    Page<Dibs> findPageWithProductByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+
 
 }
