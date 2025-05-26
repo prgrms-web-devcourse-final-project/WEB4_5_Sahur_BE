7afd8cd4774be1f0a3f4f79c35f79acf15e06be7
linedj <eunjun998@gmail.com>
2025-05-26 14:22:54 +0900

    build log

--- backend/src/main/java/com/team5/backend/domain/member/productrequest/dto/ProductRequestCreateReqDto.java
+++ backend/src/main/java/com/team5/backend/domain/member/productrequest/dto/ProductRequestCreateReqDto.java
@@ -1,11 +1,14 @@
 package com.team5.backend.domain.member.productrequest.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
