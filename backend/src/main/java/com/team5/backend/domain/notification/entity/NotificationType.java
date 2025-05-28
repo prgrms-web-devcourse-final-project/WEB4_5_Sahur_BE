7c1f0d9096088fcb0d9ce7e1a330366d51751e4b
jxxngin <wwd0203@daum.net>
2025-05-28 17:12:44 +0900

    feat: 알림 타입 및 템플릿 상황 Enum 정의

--- backend/src/main/java/com/team5/backend/domain/notification/entity/NotificationType.java
+++ backend/src/main/java/com/team5/backend/domain/notification/entity/NotificationType.java
@@ -1,7 +1,9 @@
 package com.team5.backend.domain.notification.entity;
 
 public enum NotificationType {
-    ORDER,
-    EVENT,
-    ETC,
+    ORDER,          // 주문, 배송 관련
+    REQUEST,        // 상품 요청 승인/반려
+    GROUP_BUY,      // 공동구매 종료
+    DIBS,           // 관심상품 알림
+    SYSTEM          // 그 외 시스템 공지 등
 }
--- backend/src/main/java/com/team5/backend/domain/notification/template/NotificationTemplateType.java
+++ backend/src/main/java/com/team5/backend/domain/notification/template/NotificationTemplateType.java
@@ -1,7 +1,13 @@
 package com.team5.backend.domain.notification.template;
 
 public enum NotificationTemplateType {
-    PURCHASED,       // 구매 완료
-    IN_DELIVERY,     // 배송 중
-    DELIVERY_DONE    // 배송 완료
+    PURCHASED,          // 구매 완료
+    ORDER_CANCELED,     // 주문 취소
+    IN_DELIVERY,        // 배송 중
+    DELIVERY_DONE,      // 배송 완료
+    REQUEST_APPROVED,   // 요청 승인
+    REQUEST_REJECTED,   // 요청 반려
+    DIBS_REOPENED,      // 관심 상품 재오픈
+    DIBS_DEADLINE,      // 관심 상품 마감 임박
+    GROUP_CLOSED        // 모집 종료
 }
