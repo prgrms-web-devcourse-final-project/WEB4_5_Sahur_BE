ab2114cfddb640c97db239eaaeb6476aaf7181f1
ShinYounHo <suh2963@gmail.com>
2025-05-29 16:27:11 +0900

    fix: 배포 환경에서 기본 프로필이 헤더에서 출력되지 않는 문제 수정

--- frontend/src/layout/main/components/NavigationMenu.js
+++ frontend/src/layout/main/components/NavigationMenu.js
@@ -20,7 +20,7 @@ const NavigationMenu = ({ menuItems }) => {
     const [loginUser, setLoginUser] = useRecoilState(userAtom);
     console.log(loginUser);
 
-    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
+    const API_BASE_URL = process.env.REACT_APP_API_URL || '';
 
     useEffect(() => {
         userProfileMutation.mutate();
